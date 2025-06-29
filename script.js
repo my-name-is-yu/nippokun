22c74055f8208676b266dc0186c5061f9bf4d304
yu y <yuyoshimuta1217@gmail.com>
2025-06-29 21:00:20 +0900

    CSS,JS変更

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 console.log("JavaScriptが読み込まれました！");
 
-// h1タグをクリックしたらアラートを出す
-const heading = document.querySelector('h1');
-heading.addEventListener('click', () => {
-  alert('H1がクリックされました！');
-});
+// ... (略)
+if (response.ok) {
+    // 成功した場合の処理
+    resultDiv.className = 'result-success'; // successクラスを追加
+    resultDiv.innerHTML = `...`;
+} else {
+    // エラーが発生した場合の処理
+    resultDiv.className = 'result-error'; // errorクラスを追加
+    resultDiv.innerHTML = `...`;
+}
